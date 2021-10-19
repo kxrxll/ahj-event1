d293378490e8893ae0bb479bb71095913c6fa77b
kxrxll <kxrxll@kxrxlls-MacBook-Air.local>
2021-10-19 13:52:42 +0300

    solved additional tasks

--- src/js/GoblinGame.js
+++ src/js/GoblinGame.js
@@ -3,21 +3,27 @@ import { randomIndex, insertImage } from './utils';
 export default class GoblinGame {
   constructor(el) {
     this.el = el;
+    this.gamefield = el.querySelector('.gamefield');
     this.index = randomIndex(16);
     this.defeatCounter = 0;
     this.points = 0;
+    this.missing = 0;
+    this.button = el.querySelector('button');
+    this.pointsblock = el.querySelector('.points');
+    this.missed = el.querySelector('.missed');
   }
 
   drawTable() {
-    const scriptTag = this.el.querySelector('script');
     for (let i = 0; i < 16; i += 1) {
       const newDiv = document.createElement('div');
+      newDiv.classList.add('cell');
       newDiv.dataset.index = i;
-      this.el.insertBefore(newDiv, scriptTag);
+      this.gamefield.append(newDiv);
     }
   }
 
   startGame() {
+    this.button.classList.add('hide');
     const newImg = document.createElement('img');
     const newInsertFunction = () => {
       const newRandomIndex = randomIndex(this.index);
@@ -28,17 +34,24 @@ export default class GoblinGame {
         alert(`Defeat! Your points ${this.points}!`);
         this.defeatCounter = 0;
         this.points = 0;
+        this.missing = 0;
+        document.location.reload();
       }
     };
     setInterval(newInsertFunction, 700);
   }
 
   initGame() {
-    this.el.addEventListener('click', (evt) => {
+    this.gamefield.addEventListener('click', (evt) => {
       if (evt.target.tagName === 'IMG') {
         this.points += 1;
         this.defeatCounter -= 1;
+        this.pointsblock.textContent = this.points;
+      } else {
+        this.missing += 1;
+        this.missed.textContent = this.missing;
       }
     });
+    this.button.addEventListener('click', this.startGame.bind(this));
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import GoblinGame from './GoblinGame';
 
-const goblinGame = new GoblinGame(document.querySelector('body'));
+const goblinGame = new GoblinGame(document.querySelector('.game'));
 goblinGame.drawTable();
-goblinGame.startGame();
 goblinGame.initGame();
