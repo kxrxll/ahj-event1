e68539093a85f45554d4e2489b0f75646c0b8686
kxrxll <kxrxll@kxrxlls-MacBook-Air.local>
2021-10-19 18:09:25 +0300

    bug fix

--- src/js/GoblinGame.js
+++ src/js/GoblinGame.js
@@ -4,13 +4,12 @@ export default class GoblinGame {
   constructor(el) {
     this.el = el;
     this.gamefield = el.querySelector('.gamefield');
-    this.index = randomIndex(16);
-    this.defeatCounter = 0;
-    this.points = 0;
-    this.missing = 0;
     this.button = el.querySelector('button');
     this.pointsblock = el.querySelector('.points');
     this.missed = el.querySelector('.missed');
+    this.defeatCounter = 0;
+    this.points = 0;
+    this.index = randomIndex(16);
   }
 
   drawTable() {
@@ -25,31 +24,37 @@ export default class GoblinGame {
   startGame() {
     this.button.classList.add('hide');
     const newImg = document.createElement('img');
+    insertImage(this.el, newImg, this.index);
     const newInsertFunction = () => {
       const newRandomIndex = randomIndex(this.index);
       this.index = newRandomIndex;
       insertImage(this.el, newImg, newRandomIndex);
       this.defeatCounter += 1;
-      if (this.defeatCounter > 5) {
+      this.missed.textContent = this.defeatCounter;
+      clearTimeout(this.interval);
+      this.interval = setTimeout(newInsertFunction, 2000);
+      if (this.defeatCounter >= 5) {
         alert(`Defeat! Your points ${this.points}!`);
         this.defeatCounter = 0;
         this.points = 0;
-        this.missing = 0;
         document.location.reload();
       }
     };
-    setInterval(newInsertFunction, 700);
+    this.newInsertFunction = newInsertFunction;
+    this.interval = setTimeout(newInsertFunction, 2000);
   }
 
   initGame() {
     this.gamefield.addEventListener('click', (evt) => {
       if (evt.target.tagName === 'IMG') {
         this.points += 1;
-        this.defeatCounter -= 1;
+        const newIndex = randomIndex(this.index);
+        this.index = newIndex;
+        insertImage(this.el, evt.target, newIndex);
+        clearTimeout(this.interval);
+        this.interval = setTimeout(this.newInsertFunction, 2000);
         this.pointsblock.textContent = this.points;
-      } else {
-        this.missing += 1;
-        this.missed.textContent = this.missing;
+        this.missed.textContent = this.defeatCounter;
       }
     });
     this.button.addEventListener('click', this.startGame.bind(this));
